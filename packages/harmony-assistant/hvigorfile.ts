ad243738d46eca010481af274e88be72f26cbc8b
YIIWWYII <276665380@qq.com>
2026-07-30 14:02:04 +0800

    fix(workstation): align assistant ownership and scope

--- packages/harmony-assistant/hvigorfile.ts
+++
@@ -1,7 +0,0 @@
-// @ts-nocheck - Hvigor configuration is executed by the DevEco toolchain.
-import { appTasks } from '@ohos/hvigor-ohos-plugin'
-
-export default {
-  system: appTasks,
-  plugins: []
-}
